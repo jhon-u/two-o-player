e82f31cf778cfd8e86fbbdb0ec7f90046ef2e7f9
Jhon Useche <jhon.useche@telus.com>
2023-01-23 15:12:17 -0700

    Complete Question class to initialize values and refactor the player input.

--- question.rb
+++ question.rb
@@ -2,18 +2,23 @@
 
 # Creates a new math question
 class Question
-  @number1 = rand(50)
-  @number2 = rand(50)
-  @answer = @number1 + @number2
-
-  def answer?(player_input)
-    @result == player_input
+  def initialize
+    @number1 = rand(50)
+    @number2 = rand(50)
+    @answer = @number1 + @number2
   end
 
   def question(player)
-    @question = "#{player}: What does #{@number1} plus #{@number2} equal?"
-    puts '> '
-    player_input = $stdin.gets.chomp
-    @answer == player_input
+    "#{player.name}: What does #{@number1} plus #{@number2} equal?"
+  end
+
+  def player_answer
+    print '> '
+    $stdin.gets
+  end
+
+  def answer?(player)
+    puts question(player)
+    @answer == player_answer.to_i
   end
 end
