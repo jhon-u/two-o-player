98dbe131439fada62674a62ec6777b3f27ade8cb
Jhon Useche <jhon.useche@telus.com>
2023-01-23 15:08:30 -0700

    Re-add uncommented code.

--- main.rb
+++ main.rb
@@ -3,8 +3,8 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
 require './game'
-# require './player'
-# require './question'
+require './player'
+require './question'
 
 new_game = Game.new
 new_game.start
